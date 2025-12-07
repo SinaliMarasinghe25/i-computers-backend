c7bc51468a9cedcd2656e7a4a8bf1e493377a626
SinaliMarasinghe25 <sinalimarasingheuni01@gmail.com>
2025-12-07 11:29:50 +0530

    Day 22✏️

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -3,14 +3,14 @@ import Product from "../models/Product.js";
 import { isAdmin } from "./userController.js";
 
 export async function createOrder(req, res) {
-    console.log(req.body)
+    console.log(req.body);
 	//ORD000001
-    if(req.user == null){
-        res.status(401).json({
-            message: "Unauthorized"
-        });
-        return;
-    }
+    if (req.user == null) {
+		res.status(401).json({
+			message: "Unauthorized",
+		});
+		return;
+	}
 	try {
 		const latestOrder = await Order.findOne().sort({ date: -1 });
 
@@ -27,67 +27,66 @@ export async function createOrder(req, res) {
 			orderId = "ORD" + newOrderNumberString; // "ORD000013"
 		}
 
-        const items = []
-        let total = 0
+        const items = [];
+		let total = 0;
 
+		for (let i = 0; i < req.body.items.length; i++) {
+			const product = await Product.findOne({
+				productID: req.body.items[i].productID,
+			});
 
+			if (product == null) {
+				return res.status(400).json({
+					message: `Product with ID ${req.body.items[i].productID} not found`,
+				});
+			}
 
-        for(let i = 0; i < req.body.items.length; i++){
+			//check if stock is available
+			// if(product.stock < req.body.items[i].quantity){
+			//     return res.status(400).json({
+			//         message : `Only ${product.stock} items available for product ID ${req.body.items[i].productID}`
+			//     })
+			// }
 
-            const product = await Product.findOne({productID : req.body.items[i].productID})
+			items.push({
+				productID: product.productID,
+				name: product.name,
+				price: product.price,
+				quantity: req.body.items[i].quantity,
+				image: product.images[0],
+			});
 
-            if(product == null){
-                return res.status(400).json({
-                    message : `Product with ID ${req.body.items[i].productID} not found`
-                })
-            }
-            
-            //check if stock is available
-            // if(product.stock < req.body.items[i].quantity){
-            //     return res.status(400).json({
-            //         message : `Only ${product.stock} items available for product ID ${req.body.items[i].productID}`
-            //     })
-            // }
+			total += product.price * req.body.items[i].quantity;
+		}
 
-            items.push({
-                productID : product.productID,
-                name : product.name,
-                price : product.price,
-                quantity : req.body.items[i].quantity,
-                image : product.images[0]
-            })
+		let name = req.body.name;
+		if (name == null) {
+			name = req.user.firstName + " " + req.user.lastName;
+		}
 
-            total += product.price * req.body.items[i].quantity
-        }
+		const newOrder = new Order({
+			orderId: orderId,
+			email: req.user.email,
+			name: name,
+			address: req.body.address,
+			total: total,
+			items: items,
+			phone: req.body.phone,
+		});
 
-        let name = req.body.name
-        if(name == null){
-            name = req.user.firstName + " " + req.user.lastName
-        }
+		await newOrder.save();
 
-        const newOrder = new Order({
-            orderId : orderId,
-            email : req.user.email,
-            name : name,
-            address : req.body.address,
-            total : total,
-            items : items,
-            phone : req.body.phone,
-        })
+		// for (let i = 0; i < items.length; i++){
+		//     await Product.updateOne(
+		//         { productID : items[i].productID },
+		//         { $inc : { stock : -items[i].quantity } }
+		//     )
+		// }
 
-        await newOrder.save()
-
-        // for (let i = 0; i < items.length; i++){
-        //     await Product.updateOne(
-        //         { productID : items[i].productID },
-        //         { $inc : { stock : -items[i].quantity } }
-        //     )
-        // }
-
-        return res.json({
-            message : "Order placed successfully",
-            orderId : orderId
-        })
+		return res.json({
+			message: "Order placed successfully",
+			orderId: orderId,
+		});
 
 	} catch (error) {
 		return res.status(500).json({
@@ -97,24 +96,54 @@ export async function createOrder(req, res) {
 	}
 }
 
-export async  function getOrders(req, res){
-    if(req.user == null){
-        res.status(401).json({
-            message: "Unauthorized"
-        });
-        return;
-    }
+export async function getOrders(req, res) {
+	if (req.user == null) {
+		res.status(401).json({
+			message: "Unauthorized",
+		});
+		return;
+	}
 
-    if(isAdmin(req)){
-        const orders = await Order.find().sort({date : -1})
+   if (isAdmin(req)) {
+		const orders = await Order.find().sort({ date: -1 });
 
-        res.json(orders)
 
-    }else{
+        res.json(orders);
+	} else {
+		const orders = await Order.find({ email: req.user.email }).sort({
+			date: -1,
+		});
+        
+        res.json(orders);
+	}
+}
 
-        const orders = await Order.find({email : req.user.email}).sort({date : -1})
+        export async function updateOrderStatus(req, res) {
+	if (!isAdmin(req)) {
+		res.status(401).json({
+			message: "Unauthorized",
+		});
+		return;
+	}
+	try {
+		const orderId = req.params.orderId;
+		const status = req.body.status;
+		const notes = req.body.notes;
 
-        res.json(orders)
+        await Order.updateOne(
+			{ orderId: orderId },
+			{ status: status, notes: notes }
+		);
 
-    }
+        
+	res.json({
+			message: "Order status updated successfully",
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Error updating order status",
+			error: error.message,
+		});
+	}
+    
 }
